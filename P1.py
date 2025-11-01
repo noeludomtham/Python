71ae00732aa4f6ce846679efb521ba4f3d07a39e
noeludomtham <cat.minguet@gmail.com>
2025-11-01 14:59:09 +0700

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,29 @@
+#Print sentence
+print("Welcome to the world of programming")
+#Print Number
+print(4)
+#Next line after print
+print("Hello Python \n")
+#Pass multiple arguments for printing
+print("hello", 5)
+#End argument of Print Statement
+print("Welcome to ", end= "*")
+
+#Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
+
+import keyword
+#Print all the Python keywords
+print("The Python keywords are...\n")
+print(keyword.kwlist)
