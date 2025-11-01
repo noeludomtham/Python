d2282ab5bd7638b7f26f45218b98a03f8f23527f
noeludomtham <cat.minguet@gmail.com>
2025-11-01 15:08:41 +0700

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,11 @@
+Dad = "February 6"
+Mom = "November 5"
+Tyme = "October 16"
+Leon = "July 14"
+Me = "December 9"
+
+print("My Dad's bithday is on ", Dad)
+print("My Mom's bithday is on ", Mom)
+print("Tyme's bithday is on ", Tyme)
+print("Leon's bithday is on ", Leon)
+print("My bithday is on ", Me)
