86d552845d659de8e699a409d88223dd2847bc6c
noeludomtham <cat.minguet@gmail.com>
2025-11-08 15:08:41 +0700

    Add files via upload

---
+++ String_Op.py
@@ -0,0 +1,7 @@
+text = str(input("Enter a string: "))
+
+revText = text[::-1]
+text = revText
+
+print("The reverse of the given string is: ")
+print(text)
---
+++ Typecasting.py
@@ -0,0 +1,12 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("The Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("The Data Type of weight is", type(weight))
