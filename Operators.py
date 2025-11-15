8e9977380f2852ee466a968aa400163e48702306
noeludomtham <cat.minguet@gmail.com>
2025-11-15 14:58:47 +0700

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,30 @@
+tree1 = 5
+tree2 = 8
+tree3 = 2
+tree4 = 4
+tree5 = 1
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all 5 the trees is: ", sum)
+average = sum/5
+print("the average of all the trees is: ", average)
+
+Amount =int(input("Enter the withdrawal amount:"))
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+
+print("Amount of 500 rupee banknotes:", note_1)
+print("Amount of 50 rupee banknotes:", note_2)
+print("Amount of 10 rupee banknotes:", note_3)
+
+Math = int(input("Math grades: " ))
+English = int(input("English grades: " ))
+Sports = int(input("Sports grades: " ))
+Art = int(input("Art grades: " ))
+
+sum = Math+English+Sports+Art
+print("The sum is: ", sum)
+
+perc = (sum/400)*100
+print("The percentage is: ", perc)
+print(perc)
