39f28356da4ca3c3c4fc4c5d26af0cb41fe32536
noeludomtham <cat.minguet@gmail.com>
2025-12-23 11:37:01 +0700

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,5 @@
+temp = int(input("Enter the temperature outside:"))
+if temp > 25:
+    print("The temperature is perfect of light clothes!")
+else:
+    print("It's COLD! The suitaible clothing are thick winter clothes.")
