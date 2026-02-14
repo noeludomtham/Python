b5e1e5975cb19ef1a9dbc4ef6bc6723c6e9d41fc
noeludomtham <cat.minguet@gmail.com>
2026-02-14 15:03:37 +0700

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+def factorial (x):
+    if x==0 or x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+print(factorial.__doc__)
+print("The factorial of 5 is: ",factorial(5))
+print("The factorial of 4 is: ",factorial(4))
