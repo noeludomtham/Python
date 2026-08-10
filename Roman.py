3255bd0a1bf6d4d59963679c26ca65dc0c01575b
noeludomtham <cat.minguet@gmail.com>
2026-08-10 13:43:30 +0700

    Add files via upload

---
+++ Roman.py
@@ -0,0 +1,15 @@
+r = input("Enter a Roman numeral: ")
+if r == "I":
+    print("1")
+if r == "V":
+    print("5")
+if r == "X":
+    print("10")
+if r == "L":
+    print("50")
+if r == "C":
+    print("100")
+if r == "D":
+    print("500")
+if r == "M":
+    print("1000")
